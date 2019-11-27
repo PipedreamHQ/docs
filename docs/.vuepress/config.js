06d4b34f38b14cdcf59f0cc14242ab6b4981bd9e
Dylan J. Sather <dylan.sather@gmail.com>
2019-11-27 14:50:01 -0800

    Adding Github repo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,6 +26,15 @@ module.exports = {
         link: "https://pipedream.com/"
       }
     ],
+    repo: "PipedreamHQ/docs",
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are not at the root of the repo:
+    docsDir: "docs",
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: "Help us improve this page! Submit an edit on Github",
     sidebar: [
       "/",
       "/support/",
